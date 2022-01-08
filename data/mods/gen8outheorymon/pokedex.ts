e8e49eaa35ede5bb4ba6b44d89d1fc3474ff048f
TailGlowVM <92479691+TailGlowVM@users.noreply.github.com>
2022-01-08 22:15:23 +0000

    Create pokedex.ts

---
+++ data/mods/gen8outheorymon/pokedex.ts
@@ -0,0 +1,13 @@
+   //pokemonname: {
+       //inherit: true,
+       //types: ["Type1", "Type2"],
+       //baseStats: {hp: 77, atk: 130, def: 132, spa: 72, spd: 90, spe: 70},
+       //abilities: {0: "Primary Ability", 1: "Secondary Ability", H: "Hidden Ability"},
+   //},
+
+export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
+   zarude: {
+      inherit: true,
+      abilities: {0: "Leaf Guard", 1: "Tough Claws"},
+   },
+};

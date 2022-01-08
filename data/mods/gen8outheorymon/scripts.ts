abbe416b4d88eab2d5c00a63eb9c198fde9a0294
TailGlowVM <92479691+TailGlowVM@users.noreply.github.com>
2022-01-08 22:36:41 +0000

    Create scripts.ts

---
+++ data/mods/gen8outheorymon/scripts.ts
@@ -0,0 +1,8 @@
+//this.modData('Learnsets', 'starmie').learnset.mysticalfire = [8L1];
+
+//export const Scripts: BattleScriptsData = {
+   //init: function() {
+      //this.modData('Learnsets', 'starmie').learnset.mysticalfire = [8L1];
+   //},
+//};
+  
